26b56dbaaff58559da7ab9ec3d415b63bdad91b4
Dayna Tynan <Dayna_Regan@yahoo.com>
2023-07-07 15:22:29 -0400

    added hamburger menu

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,32 +1,47 @@
 import "./Nav.css";
+import { useState } from "react";
 
 const Navigation = ({ page, setPage }) => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    console.log('Menu toggled');
+    setMenuOpen(!menuOpen);
+  }
+
   return (
-    <nav>
+    <nav className="nav">
       <img src="./pics/campinggreen.jpg" width="30" height="30" alt="Our logo"></img>
-    <section>
-       <ul>
-        <li onClick={() => setPage("home")}>
-          <a href="/home" onClick={(event) => event.preventDefault()}>
-            Home Page
-          </a>
-        </li>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <span className="menu-icon__line"></span>
+        <span className="menu-icon__line"></span>
+        <span className="menu-icon__line"></span>
+      </div>
+      <section>
+        <ul className={menuOpen ? "menu-list menu-list--active" : "menu-list"}>
+          <li onClick={() => setPage("home")}>
+            <a href="/home" onClick={(event) => event.preventDefault()}>
+              Home Page
+            </a>
+          </li>
 
-        <li onClick={() => setPage("nationalParks")}>
-          <a href="/nationalParks" onClick={(event) => event.preventDefault()}>
-            National Parks
-          </a>
-        </li>
+          <li onClick={() => setPage("nationalParks")}>
+            <a
+              href="/nationalParks"
+              onClick={(event) => event.preventDefault()}
+            >
+              National Parks
+            </a>
+          </li>
 
-        <li onClick={() => setPage("mountains")}>
-          <a href="/mountains" onClick={(event) => event.preventDefault()}>
-            Mountains Information
-          </a>
-        </li>
-      </ul>
-    </section>
+          <li onClick={() => setPage("mountains")}>
+            <a href="/mountains" onClick={(event) => event.preventDefault()}>
+              Mountains Information
+            </a>
+          </li>
+        </ul>
+      </section>
     </nav>
-    
   );
 };
 export default Navigation;
